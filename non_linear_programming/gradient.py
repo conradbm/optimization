84df066764d7d24be40cb44ef7dfc9b3749b82b6
Blake Conrad <bmconrad@iupui.edu>
2018-04-01 17:23:03 -0500

    Update gradient.py

--- non_linear_programming/gradient.py
+++ non_linear_programming/gradient.py
@@ -29,3 +29,20 @@ eps=0.001
     #l=10
     #eval(eq1) #evaluated the derivative of the original function substituted for lambda @ a value of lambda.
 # } While ( | xk[k] - xk[k-1 | > eps  )
+
+
+# better way
+import sympy
+x0 = (0,0)
+x, y, lamda = sympy.symbols('x y lamda')
+f = 3*x**2+x*y+y**2+x+2
+fgradient = [f.diff(var) for var in (x, y)]   # calling diff as a method is convenient
+# do{
+xk = x0
+direction = [i.subs({x: xk[0], y: xk[1]}) for i in fgradient]
+xk = [xk[i] - lamda*direction[i] for i in range(len(direction))]
+flamda = f.evalf(subs={x: xk[0], y: xk[1]})
+dflambda = flambda.diff()
+lam = solve(dflambda) #assuming only 1 lambda
+xk = [i.subs({lamda:lam}) for i in xk]#untested
+#flamda = sympy.lambdify(lamda, f.subs({x: xk[0], y: xk[1]}))
