9cb78815e122250958ed0da9da5967331406368e
Blake Conrad <bmconrad@iupui.edu>
2018-04-01 18:40:25 -0500

    Update gradient.py

--- non_linear_programming/gradient.py
+++ non_linear_programming/gradient.py
@@ -1,48 +1,39 @@
-import simpy
-
-# Remarks: xk should be a list, x0 should be initially appended, then xk should be appended at the end of each iteration
-
-# Initialize:
-x0=(0,0)
-f= simpy.Eq(3*x**2+x*y+y**2+x+2)
-str_vars = [str(a) for a in f.atoms() if not str(a).isdigit()]
-vars  = [a for a in f.atoms() if not str(a).isdigit()]
-
-f=3*x**2+x*y+y**2+x+2
-fgradient=[sympy.diff(f,var) for var in vars]
-x0
-eps=0.001
-
-# do {
-    direction=[i.evalf(subs={'x':x0[0],’y’:x0[1]}) for i in fgradient]
-    direction_lambda = [Symbol('lambda')*val for val in direction]
-
-    xk = [direction[i]-direction_lambda[i] for i in range(len(direction))]
-    flambda  = f.subs({x:xk[0], y:xk[1]})
-
-    dflambda = flambda.diff()
-    lam = solve(dflambda) #assuming only 1 lambda
-    xk = [str(comp).replace("lambda",str(lam[0])) for comp in xk]
-    k+=1
-    #eq1 = str(dflambda)
-    #eq1 = eq1.replace("lambda", "l")
-    #l=10
-    #eval(eq1) #evaluated the derivative of the original function substituted for lambda @ a value of lambda.
-# } While ( | xk[k] - xk[k-1 | > eps  )
-
-
-# better way
 import sympy
-x0 = (0,0)
+import numpy as np
+
+## Consider a two-dimensional problem.
+
+# Initialization:
+x0 = np.random.randint(1,100, size=2)
+epsilon = 0.001
+k=0
 x, y, lamda = sympy.symbols('x y lamda')
 f = 3*x**2+x*y+y**2+x+2
 fgradient = [f.diff(var) for var in (x, y)]   # calling diff as a method is convenient
-# do{
-xk = x0
-direction = [i.subs({x: xk[0], y: xk[1]}) for i in fgradient]
-xk = [xk[i] - lamda*direction[i] for i in range(len(direction))]
-flamda = f.evalf(subs={x: xk[0], y: xk[1]})
-dflambda = flambda.diff()
-lam = solve(dflambda) #assuming only 1 lambda
-xk = [i.subs({lamda:lam}) for i in xk]#untested
-#flamda = sympy.lambdify(lamda, f.subs({x: xk[0], y: xk[1]}))
+xk=[]
+xk.append(x0)
+k+=1
+print(xk)
+
+# Procedure:
+for i in range(1000):
+    
+    direction = [i.subs({x: xk[k-1][0], y: xk[k-1][1]}) for i in fgradient]
+    xi = [xk[k-1][i] - lamda*direction[i] for i in range(len(direction))]
+    flamda  = f.subs({x:xi[0], y:xi[1]})
+    dflamda = flamda.diff()
+    soln = sympy.solve(dflamda)[0] #Assuming lambda has only 1 root, so it is the best 
+    xi = [i.subs({lamda:soln}).evalf() for i in xi]
+    xk.append(np.array(xi))
+
+    print("k-1: ",xk[k-1])
+    print("k: ",xk[k])
+    print("difference: ",abs(xk[k] - xk[k-1]))
+    print ("Which elements are less than epsilon? ",abs(xk[k] - xk[k-1]) < epsilon)
+    print("Are all less than epsilon? ", all(abs(xk[k] - xk[k-1]) < epsilon))
+    if all(abs(xk[k] - xk[k-1]) < epsilon):
+        break
+    k= k + 1
+local_optimum = xk[-1] #and if convex, it is global
+local_optimum
+# >> Soln: array([-0.181810432795123, 0.0908748033628853], dtype=object)
